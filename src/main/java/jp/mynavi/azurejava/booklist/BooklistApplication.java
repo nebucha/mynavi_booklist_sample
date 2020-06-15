5323b8b977482d5ad932ee9197b75c6692342516
nebucha <takasyou@gmail.com>
2020-06-15 17:42:27 +0900

    Add a comment.

--- src/main/java/jp/mynavi/azurejava/booklist/BooklistApplication.java
+++ src/main/java/jp/mynavi/azurejava/booklist/BooklistApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BooklistApplication {
 
 	public static void main(String[] args) {
+		// ベースアプリケーションの実行
 		SpringApplication.run(BooklistApplication.class, args);
 	}
 
